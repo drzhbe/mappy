604e0ceeb4d959d5aa073bb2c639c37b90a6951c
Stanislav Salnikov <stanislav.s@canva.com>
2019-10-04 17:54:02 +1000

    add zoom level switch by keyboard - +

--- src/index.js
+++ src/index.js
@@ -1,12 +1,42 @@
 class Map {
   constructor(o) {
     this.container = o.container;
-    this.zoom = 3;
+    this.MIN_ZOOM = o.minZoom || 0;
+    this.MAX_ZOOM = o.maxZoom || 3;
+    this.zoom = o.zoom || this.MAX_ZOOM;
     this.tileSize = o.tileSize || 256;
+
+    document.addEventListener('keydown', e => {
+      switch (e.key) {
+        case '+':
+        case '=': // handle "unshifted" `+` key too
+          this.zoomIn();
+          break;
+        case '-':
+          this.zoomOut();
+          break;
+      }
+    });
 
     this.loadTiles();
   }
+  zoomIn() {
+    this.setZoom(this.zoom + 1);
+  }
+  zoomOut() {
+    this.setZoom(this.zoom - 1);
+  }
+  setZoom(zoom) {
+    this.zoom = Math.max(this.MIN_ZOOM, Math.min(zoom, this.MAX_ZOOM));
+    this.loadTiles();
+  }
   loadTiles() {
+    // Clear the old tiles.
+    while (this.container.firstChild) {
+      this.container.firstChild.remove();
+    }
+
+    // Load new tiles.
     const areas = 2 ** this.zoom;
     for (let i = 0; i < areas; i++) {
       for (let j = 0; j < areas; j++) {
