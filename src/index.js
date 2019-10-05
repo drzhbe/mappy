d869963786f79d375b01cd88e0bb8e393ec7ba29
drzhbe <drzhbe@gmail.com>
2019-10-05 15:56:12 +1000

    add dragndrop

--- src/index.js
+++ src/index.js
@@ -2,4 +2,8 @@ import { Map } from './map.js';
 
 const map = new Map({
   container: document.getElementById('app'),
+  getViewport: () => ({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  }),
 });
--- src/map.js
+++ src/map.js
@@ -1,10 +1,30 @@
+/**
+ * For x zoom level we have 2 ** x columns, each column has 2 ** x tiles.
+ *
+ * Zoom=2 looks like:
+ *  ___________________________
+ * |      |      |      |      |
+ * | c0t0 | c1t0 | c2t0 | c3t0 |
+ * |______|______|______|______|
+ * |      |      |      |      |
+ * | c0t1 | c1t1 | c2t1 | c3t1 |
+ * |______|______|______|______|
+ * |      |      |      |      |
+ * | c0t2 | c1t2 | c2t3 | c3t2 |
+ * |______|______|______|______|
+ * |      |      |      |      |
+ * | c0t3 | c1t3 | c2t4 | c3t3 |
+ * |______|______|______|______|
+ */
 export class Map {
   constructor(o) {
     this.container = o.container;
+    this.getViewport = o.getViewport;
     this.MIN_ZOOM = o.minZoom || 0;
     this.MAX_ZOOM = o.maxZoom || 3;
     this.zoom = o.zoom || this.MAX_ZOOM;
     this.tileSize = o.tileSize || 256;
+    this.center = { x: 0.5, y: 0.5 };
 
     document.addEventListener('keydown', e => {
       switch (e.key) {
@@ -18,7 +38,30 @@ export class Map {
       }
     });
 
+    // Disable browser's default image dragging.
+    document.addEventListener('mousedown', e => e.preventDefault());
+    // Move the map when left mouse button is pressed.
+    document.addEventListener('mousemove', e => e.buttons === 1 && this.moveBy(e.movementX, e.movementY));
+
     this.loadTiles();
+    this.updateView();
+  }
+  moveBy(dx, dy) {
+    const mapSide = this.mapSide;
+    const viewport = this.getViewport();
+    // Use center.x `-` dx (inverse dragging) to imitate spinning the globe with your hand,
+    // rather than `+` as in standard operating-system-like drag'n'drop with the mouse cursor.
+    this.center.x = clamp(
+        0,
+        this.center.x - this.normalize(dx),
+        1 - this.normalize(viewport.width) / 2,
+    );
+    this.center.y = clamp(
+        0,
+        this.center.y - this.normalize(dy),
+        1 - this.normalize(viewport.height) / 2,
+    );
+    this.updateView();
   }
   zoomIn() {
     this.setZoom(this.zoom + 1);
@@ -27,8 +70,42 @@ export class Map {
     this.setZoom(this.zoom - 1);
   }
   setZoom(zoom) {
-    this.zoom = Math.max(this.MIN_ZOOM, Math.min(zoom, this.MAX_ZOOM));
+    this.zoom = clamp(this.MIN_ZOOM, zoom, this.MAX_ZOOM);
     this.loadTiles();
+    this.updateView();
+  }
+  /**
+   * Normalizes the pixel coordinate value relative to map side (1200px => 0.5).
+   */
+  normalize(value) {
+    return value / this.mapSide;
+  }
+  /**
+   * Denormalizes the value (0-1) relative to map side into real px value (0.5 => 1200px).
+   */
+  denormalize(value) {
+    return value * this.mapSide;
+  }
+  /**
+   * Calculates how many tiles per zoom level there is.
+   * Tiles-per-column is the same number as columns-per-zoom-level is.
+   */
+  get tilesCount() {
+    return 2 ** this.zoom;
+  }
+  /**
+   * Assuming that map is a square, calculates the side of the map.
+   */
+  get mapSide() {
+    return this.tilesCount * this.tileSize;
+  }
+  updateView() {
+    const mapSide = this.mapSide;
+    const v = this.getViewport();
+    const x = clamp(0, this.denormalize(this.center.x) - v.width / 2, mapSide);
+    const y = clamp(0, this.denormalize(this.center.y) - v.height / 2, mapSide);
+    // Negate translate values to have inverse dragging.
+    this.container.style.transform = `translate(-${x}px, -${y}px)`;
   }
   loadTiles() {
     // Clear the old tiles.
@@ -37,9 +114,9 @@ export class Map {
     }
 
     // Load new tiles.
-    const areas = 2 ** this.zoom;
-    for (let i = 0; i < areas; i++) {
-      for (let j = 0; j < areas; j++) {
+    const tiles = this.tilesCount;
+    for (let i = 0; i < tiles; i++) {
+      for (let j = 0; j < tiles; j++) {
         const img = new Image();
         img.src = `../assets/tiled/${this.zoom}/${i}/${j}.jpg`;
         img.classList.add('map__tile');
@@ -48,4 +125,8 @@ export class Map {
       }
     }
   }
+}
+
+function clamp(min, v, max) {
+  return Math.max(min, Math.min(v, max));
 }
